7d5ffd574edbcdf8b822f9164f868c9112a50e96
Macbook Air <mac@Amars-MacBook-Air.local>
2026-03-04 00:29:13 +0530

    bug fix - userApi.js Duplicate Methods

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,30 +1,26 @@
 import apiClient from './apiClient';
 
 export const userApi = {
-  updateProfile: async (profileData) => {
-    const response = await apiClient.put('/users/profile', profileData);
-    return response.data;
-  },
-  setPin: async (pin) => {
-    const response = await apiClient.post('/users/set-pin', { new_pin: pin });
-    return response.data;
-  },
-  verifyPin: async (pin) => {
-    const response = await apiClient.post('/users/verify-pin', { pin });
-    return response.data;
-  },
   getProfile: async () => {
     const { data } = await apiClient.get('/users/profile');
     return data;
   },
+
   updateProfile: async (payload) => {
     const { data } = await apiClient.put('/users/profile', payload);
     return data;
   },
+
   setPin: async (new_pin) => {
     const { data } = await apiClient.post('/users/set-pin', { new_pin });
     return data;
   },
+
+  verifyPin: async (pin) => {
+    const { data } = await apiClient.post('/users/verify-pin', { pin });
+    return data;
+  },
+
   deleteAccount: async () => {
     const { data } = await apiClient.delete('/users');
     return data;
